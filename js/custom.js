1b0822ba4ec293c9672d2b89f1fc7ec4b868398a
evgenkarp01 <ewal@list.ru>
2019-04-21 22:29:10 +0300

    21042019

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,31 @@
 $( document ).ready(function() {
 
+    $(".filter_btn a").click(this, function(e){
+            e.preventDefault();
+            $('.filters').find(".filter__hidden").toggle(500);	
+        })
 
+        var men=true;
+        var glschet =1;
+        $("#sandwich").click(function()
+        {
+            if(men==false)
+            {
+                $(".sub__menu").slideUp();
+                men=!men;
+            }
+            else
+            {
+                $(".sub__menu").slideDown();
+                men=!men;
+            }
+        });
+            $("#sandwich, .menu_item").click(function() {
+             $("#sandwich").toggleClass("active");
+        });
+        $(".sub__menu ul li").click(this, function(){
+
+                $(this).find("ul").toggle(500);	
+        })
 
 })
